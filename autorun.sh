aefa0ed6dce088bc10510979f5a6794f060d8a06
flaviostutz <flaviostutz@gmail.com>
2017-02-28 14:36:17 +0000

    added autorun.sh

---
+++ autorun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo "================"
+date
+echo "Calling step3..."
+
+jupyter nbconvert --to notebook --nbformat 1 --ExecutePreprocessor.timeout=-1 --execute datascience-snippets/ipython-notebooks/kaggle-lung-cancer-detection/step03-preparation-mask-center-resize-rotate.ipynb
+
+echo "Done."
+date
+echo "================"
+echo ""
