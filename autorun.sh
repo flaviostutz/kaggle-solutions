2a34174a069573fe06b4bceca588ac882ca62907
flaviostutz <flaviostutz@gmail.com>
2017-02-28 14:58:23 +0000

    fix

--- autorun.sh
+++ autorun.sh
@@ -3,7 +3,7 @@
 echo ""
 echo "================"
 date
-echo "Calling step3..."
+echo "Calling step3 script..."
 
 jupyter nbconvert --to notebook --nbformat 1 --ExecutePreprocessor.timeout=-1 --execute datascience-snippets/ipython-notebooks/kaggle-lung-cancer-detection/step03-preparation-mask-center-resize-rotate.ipynb
 
