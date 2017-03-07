6164b707e7c0c61668204daed10464c4213429e0
flaviostutz <flaviostutz@gmail.com>
2017-03-07 12:02:02 +0000

    updated autorun to step10

--- autorun.sh
+++ autorun.sh
@@ -5,7 +5,7 @@ echo "================"
 date
 echo "Calling step3 script..."
 
-jupyter nbconvert --to notebook --nbformat 1 --ExecutePreprocessor.timeout=-1 --execute datascience-snippets/ipython-notebooks/kaggle-lung-cancer-detection/step03-preparation-mask-center-resize-rotate.ipynb
+jupyter nbconvert --to notebook --nbformat 1 --ExecutePreprocessor.timeout=-1 --execute datascience-snippets/ipython-notebooks/kaggle-lung-cancer-detection/step10-train-local1.ipynb
 
 echo "Done."
 date
