4b4017a866644083b4dea247ae78a81c98394657
flaviostutz <flaviostutz@gmail.com>
2017-04-01 20:24:46 -0300

    export lions to dataset

---
+++ kaggle-sea-lion/modules/lions.py
@@ -0,0 +1,104 @@
+import modules.utils as utils
+import numpy as np
+import cv2
+from tflearn.data_utils import to_categorical
+from modules.logging import logger
+
+#colors in bgr reference
+ADULT_MALES = 0
+SUBADULT_MALE = 1
+ADULT_FEMALE = 2
+JUVENILE = 3
+PUP = 4
+
+#each index is a min/max color for a class mark
+C_MIN = [
+            np.array([0, 0, 160]),
+            np.array([200, 0, 200]),
+            np.array([10, 40, 75]),
+            np.array([150, 40, 0]),
+            np.array([25, 140, 40])
+        ]
+
+C_MAX = [
+            np.array([50, 50, 255]),
+            np.array([255, 55, 255]),
+            np.array([20, 55, 130]),
+            np.array([255, 80, 40]),
+            np.array([50, 255, 65])
+        ]
+
+def find_class(image, point):
+    image = image[point[1]-3:point[1]+3,point[0]-3:point[0]+3]
+    result = 'none'
+    max = 0
+    for col in range(5):
+        cmsk = cv2.inRange(image, C_MIN[col], C_MAX[col])
+        sm = np.sum(cmsk)
+        if(sm>max):
+            max = sm
+            result = col
+    return result
+
+
+def export_lions(image_raw, image_dotted, target_x_ds, target_y_ds, image_dims, patch_size=50, debug=False):
+   
+    psize = round(patch_size/2)
+
+    #BLACKOUT PORTIONS OF IMAGE IN RAW PICTURE
+    image_dotted_bw = cv2.cvtColor(image_dotted, cv2.COLOR_BGR2GRAY)
+    #utils.show_image(image_dotted_bw, size=8)
+
+    mask = cv2.threshold(image_dotted_bw, 5, 255, cv2.THRESH_BINARY)[1]
+    #utils.show_image(mask, size=8)
+
+    image_raw_bw = cv2.cvtColor(image_raw, cv2.COLOR_BGR2GRAY)
+    image_raw = cv2.bitwise_and(image_raw, image_raw, mask=mask)
+    #utils.show_image(image_raw, size=8, is_bgr=True)
+
+    
+    #ISOLATE HUMAN MARKS ON DOTTED PICTURE
+    diff_color = cv2.absdiff(image_dotted, image_raw)
+    diff = cv2.cvtColor(diff_color, cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((2,2),np.uint8)
+    diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernel)
+    ret,diff = cv2.threshold(diff,10,255,cv2.THRESH_TOZERO)
+    ret,diff = cv2.threshold(diff,0,255,cv2.THRESH_BINARY)
+
+    #debug data
+    debug_image = image_dotted.copy()
+    images = []
+    
+    count = 0
+    count_class = np.zeros(5)
+    im2, contours, hierarchy = cv2.findContours(diff, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        if(w>4 and h>4):
+            count = count + 1
+            center = (x+round(w/3),y+round(h/3))
+            clazz = find_class(image_dotted, center)
+            #logger.info('found=' + str(clazz))
+            count_class[clazz] = count_class[clazz] + 1
+
+            #export patch to train dataset
+            #logger.info('export x, y to dataset. count=' + str(count))
+            trainX = utils.crop_image_fill(image_raw, (center[1]-psize,center[0]-psize), (center[1]+psize,center[0]+psize))
+            target_x_ds.resize((count, image_dims[0], image_dims[1], image_dims[2]))
+            target_x_ds[count-1:count] = trainX
+            
+            trainY = to_categorical([clazz], nb_classes=5)
+            target_y_ds.resize((count, 5))
+            target_y_ds[count-1:count] = trainY
+            
+            if(debug):
+                images.append(trainX)
+                cv2.circle(debug_image,center,round(w/2),(0,0,255),1)
+
+    if(debug):
+        utils.show_image(debug_image, size=8, is_bgr=True)
+        utils.show_images(images, cols=14, is_bgr=True, size=1)
+        logger.info('total animals found: ' + str(count))
+    
+    return count_class
--- kaggle-sea-lion/modules/utils.py
+++ kaggle-sea-lion/modules/utils.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import shutil
+import os
+from time import time
+
+from modules.logging import logger
+
+#this function crops image and handles edge conditions
+def crop_image_fill(image, p1, p2, fill_color=127):
+    s = np.shape(image)
+    nim = np.full((p2[0]-p1[0], p2[1]-p1[1], 3), 127, dtype=np.uint8)
+    cim = image[p1[0]:p2[0],p1[1]:p2[1]]
+    sc = np.shape(cim)
+    nim[0:sc[0],0:sc[1]] = cim
+    return nim
 
 def show_image(pixels, output_file=None, size=6, is_bgr=False, cmap=None):
     fig1, ax1 = plt.subplots(1)
@@ -15,3 +29,60 @@ def show_image(pixels, output_file=None, size=6, is_bgr=False, cmap=None):
         plt.close(fig1)
     else:
         plt.show()
+    
+#def show_slices(pixels, name, nr_slices=12, cols=4, output_dir=None, size=7):
+def show_images(image_list, cols=4, name='image', output_dir=None, is_bgr=False, cmap=None, size=6):
+    fig = plt.figure()
+    rows = round(len(image_list)/cols)+1
+    t = Timer('generating image patches. rows=' + str(rows) + '; cols=' + str(cols))
+    fig.set_size_inches(cols*size, rows*size)
+    for i,im in enumerate(image_list):
+        if(is_bgr):
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        y = fig.add_subplot(rows,cols,i+1)
+        y.imshow(im, cmap=cmap)
+
+    if(output_dir!=None):
+        f = output_dir + name + '.jpg'
+        plt.savefig(f)
+        plt.close(fig)
+    else:
+        plt.show()
+        
+    t.stop()
+
+def dataset_name(name, image_dims):
+    return '{}-{}-{}.h5'.format(name, image_dims[0], image_dims[1])
+
+class Timer:
+    def __init__(self, name, debug=True):
+        self._name = name
+        self._debug = debug
+        self.start()
+    
+    def start(self):
+        self._start = time()
+        if(self._debug):
+            logger.info('> [started] ' + self._name + '...')
+
+    def stop(self):
+        self._lastElapsed = (time()-self._start)
+        if(self._debug):
+            logger.info('> [done]    {} ({:.3f} ms)'.format(self._name, self._lastElapsed*1000))
+            
+    def elapsed(self):
+        if(self._lastElapsed != None):
+            return (self._lastElapsed)
+        else:
+            return (time()-self._start)
+
+def mkdirs(base_dir, dirs=[], recreate=False):
+    if(recreate):
+        shutil.rmtree(base_dir, True)
+
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    
+    for d in dirs:
+        if not os.path.exists(base_dir + d):
+            os.makedirs(base_dir + d)
