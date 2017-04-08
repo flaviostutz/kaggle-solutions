7b6b8f9260259841f6fedea5433d74a9ea537309
flaviostutz <flaviostutz@gmail.com>
2017-04-08 22:55:22 +0000

    autobalance tests

--- kaggle-sea-lion/modules/utils.py
+++ kaggle-sea-lion/modules/utils.py
@@ -44,7 +44,7 @@ def class_distribution(Y_categorical):
     count_classes = np.zeros(nr_classes)
     labels = categorical_to_label(Y_categorical)
     for y in labels:
-        count_classes[y] = count_classes[y] + 1
+        count_classes[y] = count_classes[y] + 2
     return count_classes
 
 def create_xy_dataset(h5file, x_dims, y_dims):
@@ -60,122 +60,116 @@ def create_xy_dataset(h5file, x_dims, y_dims):
     return x_ds, y_ds
 
 #max_augmentation_rotation=20, max_augmentation_shift=0, max_augmentation_scale=1, augmentation_flip_leftright=True, augmentation_flip_updown=True
-def dataset_xy_balance_classes_image(input_h5file_path, output_h5file_path, max_augmentation_ratio=3, max_undersampling_ratio=1, classes_weight=1, image_data_generator=None):
-    if(os.path.isfile(output_h5file_path)):
-        raise Exception('Output file already exists. file=' + output_h5file_path)
-
+def dataset_xy_balance_classes_image(input_h5file, output_h5file, max_augmentation_ratio=3, max_undersampling_ratio=1, classes_distribution_weight=1, enforce_max_ratios=False, image_data_generator=None):
     if(image_data_generator==None):
         image_data_generator = ImageDataGenerator(
-            rotation_range=90,
+            rotation_range=360,
             fill_mode='wrap',
             cval=127,
             data_format=K.image_data_format())
 
-    logger.info('loading input dataset ' + input_h5file_path)
-    input_h5 = h5py.File(input_h5file_path, 'r')
-    with input_h5:
-        input_x_ds = input_h5['X'][50:80]#FIXME remove later
-        input_y_ds = input_h5['Y'][50:80]#FIXME remove later
-        if(len(input_x_ds)==0):
-            raise Exception('No data found on input dataset')
-        x_dims = input_x_ds.shape[1:]
-        y_dims = input_y_ds.shape[1:]
+    input_x_ds = input_h5file['X']
+    input_y_ds = input_h5file['Y']
+    if(len(input_x_ds)==0):
+        raise Exception('No data found on input dataset')
+    x_dims = input_x_ds.shape[1:]
+    y_dims = input_y_ds.shape[1:]
 
-        nr_classes = input_y_ds.shape[1]
+    nr_classes = input_y_ds.shape[1]
 
-        t = Timer('traversing entire dataset in order to extract population classes distribution')
-        count_classes = class_distribution(input_y_ds[()])
-        t.stop()
+    t = Timer('traversing entire dataset in order to extract population classes distribution')
+    count_classes = class_distribution(input_y_ds[()])
+    t.stop()
 
-        logger.info('population distribution')
-        smallest_class = None
-        smallest_qtty = 999999999
-        largest_class = None
-        largest_qtty = 0
-        for i,c in enumerate(count_classes):
-            logger.info(str(i) + ': ' + str(c))
-            if(c<smallest_qtty):
-                smallest_qtty = c
-                smallest_class = i
-            if(c>largest_qtty):
-                largest_qtty = c
-                largest_class = i
+    logger.info('population distribution')
+    smallest_class = None
+    smallest_qtty = 999999999
+    largest_class = None
+    largest_qtty = 0
+    for i,c in enumerate(count_classes):
+        logger.info(str(i) + ': ' + str(c))
+        if(c<smallest_qtty):
+            smallest_qtty = c
+            smallest_class = i
+        if(c>largest_qtty):
+            largest_qtty = c
+            largest_class = i
+    
+    minq = largest_qtty - largest_qtty*max_undersampling_ratio
+    maxq = smallest_qtty + smallest_qtty*max_augmentation_ratio
 
-        qtty_per_class = min(smallest_qtty*(1-max_augmentation_ratio), largest_qtty)
-        logger.info('targeting items per class: ' + str(qtty_per_class))
+    qtty_per_class = max(minq, maxq)
+    logger.info('targeting items per class: ' + str(qtty_per_class))
 
-        logger.info('augmentation/undersampling ratio per class')
-        ratio_classes = np.zeros(nr_classes)
-        for i,c in enumerate(count_classes):
-            if(c==0):
-                raise Exception('Class ' + str(i) + ' has zero samples. Aborting class balancing')
-            ratio_classes[i] = qtty_per_class/c
+    logger.info('augmentation/undersampling ratio per class')
+    ratio_classes = np.zeros(nr_classes)
+    for i,c in enumerate(count_classes):
+        if(c==0):
+            raise Exception('Class ' + str(i) + ' has zero samples. Aborting class balancing')
+        ratio_classes[i] = qtty_per_class/c
+        if(enforce_max_ratios):
             if(ratio_classes[i]<1):
                 ratio_classes[i] = max((1-max_undersampling_ratio), ratio_classes[i])
-            logger.info(str(i) + ': ' + str(ratio_classes[i]))
+            elif(ratio_classes[i]>1):
+                ratio_classes[i] = min(1+max_augmentation_ratio, ratio_classes[i])
+        logger.info(str(i) + ': ' + str(ratio_classes[i]))
 
-        ratio_classes = classes_weight * ratio_classes
+    ratio_classes = classes_distribution_weight * ratio_classes
+    logger.info(str(i) + ': ' + str(ratio_classes[i]))
 
-        logger.info('creating output dataset ' + output_h5file_path)
-        output_h5 = h5py.File(output_h5file_path, 'w')
-        with output_h5:
-            output_x_ds, output_y_ds = create_xy_dataset(output_h5, x_dims, y_dims)
+    output_x_ds, output_y_ds = create_xy_dataset(output_h5file, x_dims, y_dims)
 
-            logger.info('iterating over input dataset for generating a new balanced dataset using undersampling and/or augmentation')
-            y_labels = categorical_to_label(input_y_ds)
+    logger.info('iterating over input dataset for generating a new balanced dataset using undersampling and/or augmentation')
+    y_labels = categorical_to_label(input_y_ds[()])
+
+    pending_augmentations = np.zeros(nr_classes)
             
-            pending_augmentations = np.zeros(nr_classes)
-            
-            for i,x in enumerate(input_x_ds):
-                y = input_y_ds[i]
-                #x = input_x_ds[i]
-                label = y_labels[i]
-                r = ratio_classes[label]
+    for i,x in enumerate(input_x_ds):
+        y = input_y_ds[i]
+        #x = input_x_ds[i]
+        label = y_labels[i]
+        r = ratio_classes[label]
 
-                #add sample
-                if(r==1):
-                    print('add sample')
-                    add_sample_to_dataset(output_x_ds, output_y_ds, x, y)
-                
-                #undersample
-                if(r<1):
-                    #accept sample at the rate it should so we balance classes
-                    print('undersample')
-                    rdm = random.random()
-                    if(rdm<=r):
-                        add_sample_to_dataset(output_x_ds, output_y_ds, x, y)
+        #add sample
+        if(r==1):
+            add_sample_to_dataset(output_x_ds, output_y_ds, x, y)
+               
+        #undersample
+        if(r<1):
+            #accept sample at the rate it should so we balance classes
+            rdm = random.random()
+            if(rdm<=r):
+                add_sample_to_dataset(output_x_ds, output_y_ds, x, y)
 
-                #augmentation
-                elif(r>1):
+        #augmentation
+        elif(r>1):
+            #accept sample
+            add_sample_to_dataset(output_x_ds, output_y_ds, x, y)
+            pending_augmentations[label] = pending_augmentations[label] + (r-1)
 
-                    print('augmentation')
-                    #accept sample
-                    add_sample_to_dataset(output_x_ds, output_y_ds, x, y)
-                    pending_augmentations[label] = pending_augmentations[label] + r
+            pending = int(round(pending_augmentations[label]))
 
-                    pending = int(round(pending_augmentations[label]))
+            #generate augmented copies of images so we balance classes
+            if(pending>0):
+                x1 = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+                x_orig = np.array([x1])
+                y_orig = np.array([y])
 
-                    #generate augmented copies of images so we balance classes
-                    if(pending>0):
-                        x1 = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
-                        x_orig = np.array([x1])
-                        y_orig = np.array([y])
+                #show_image(x_orig[0], is_bgr=False)
+                ir = image_data_generator.flow(x_orig, y_orig, batch_size=1)
+                for i in range(pending):
+                    it = ir.next()
+                    x_it = it[0][0]
+                    y_it = it[1]
+                    x_it = cv2.cvtColor(x_it, cv2.COLOR_RGB2BGR)
+                    add_sample_to_dataset(output_x_ds, output_y_ds, x_it, y_it)
+                    #show_image(x_it, is_bgr=True)
 
-                        #show_image(x_orig[0], is_bgr=False)
-                        ir = image_data_generator.flow(x_orig, y_orig, batch_size=1)
-                        for i in range(pending):
-                            it = ir.next()
-                            x_it = it[0][0]
-                            y_it = it[1]
-                            x_it = cv2.cvtColor(x_it, cv2.COLOR_RGB2BGR)
-                            add_sample_to_dataset(output_x_ds, output_y_ds, x_it, y_it)
-                            #show_image(x_it, is_bgr=True)
-
-                    pending_augmentations[label] = pending_augmentations[label] - pending
-                        
+            pending_augmentations[label] = pending_augmentations[label] - pending
+    
     logger.info('done')
     
-    
+
 def plot_confusion_matrix(cm, class_labels=None,
                           normalize=False,
                           title='Confusion matrix',
@@ -244,6 +238,7 @@ def show_image(pixels, output_file=None, size=6, is_bgr=False, cmap=None):
 
 #def show_slices(pixels, name, nr_slices=12, cols=4, output_dir=None, size=7):
 def show_images(image_list, image_labels=None, cols=4, name='image', output_dir=None, is_bgr=False, cmap=None, size=6):
+    logger.info('showing ' + str(len(image_list)) + ' images')
     fig = plt.figure()
     rows = round(len(image_list)/cols)+1
     t = Timer('generating image patches. rows=' + str(rows) + '; cols=' + str(cols))
